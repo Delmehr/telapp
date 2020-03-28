766bc356e09c893caaea9e6405e66b30e7665e61
Delmehr <32702361+Delmehr@users.noreply.github.com>
2020-03-28 09:38:34 +0430

    some imports are done

---
+++ views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+# Create your views here.
+
+from telapp.models import Person, Telnum
+
+def index(request):
+    all_persons = Person.objects.all()
+    all_telnum = Telnum.objects.all()
+    paginator = Paginator(all_persons, 2)
+    page = request.GET.get('page')
+    persons = paginator.get_page(page)
+    context = {
+         'persons': persons,
+        #'person_list': all_persons,
+        'tel_list': all_telnum,
+        'page_title': "List of all Persons"
+
+    }
+    return render(request, "telapp/index.html", context)
+
+def list(request):
+
+    all_persons = Person.objects.all()
+    all_telnum =  Telnum.objects.all()
+
+    context = {
+
+    'person_list': all_persons,
+    'tel_list': all_telnum,
+
+    }
+
+    return render(request, "telapp/testindex.html", context)
+
+
+
+
+def search(request):
+
+
+    if request.method=='POST':
+        srch = request.POST['srh']
+        if srch:
+            match = Person.objects.filter(Q(f_name__icontains=srch) |
+                                          Q(l_name__icontains=srch))
+            all_tels = Telnum.objects.all()
+
+            if match:
+                return render(request, 'telapp/search.html', {'sr':match, 'tl':all_tels})
+            else:
+                messages.error(request,  'no result found!')
+
+        else:
+            return HttpResponseRedirect('/search/')
+
+    return render(request, 'telapp/search.html')
